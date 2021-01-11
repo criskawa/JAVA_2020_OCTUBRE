7d158d08f82d3b7de19ab88bd0edb42c2f4a9fe3
Javier Lete <javier@eskura21.com>
2021-01-11 15:32:51 +0100

    Inicio proyecto springformularios

---
+++ springformularios/src/main/java/com/ipartek/formacion/spring/springmvcbasico/SpringformulariosApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springmvcbasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringformulariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringformulariosApplication.class, args);
+	}
+
+}
