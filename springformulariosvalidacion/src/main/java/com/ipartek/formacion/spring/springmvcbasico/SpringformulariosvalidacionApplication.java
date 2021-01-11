b3a2a9ca33115c8f6d47e3d7662f7a57a999a10b
Javier Lete <javier@eskura21.com>
2021-01-11 16:56:16 +0100

    Inicio springformulariosvalidacion

---
+++ springformulariosvalidacion/src/main/java/com/ipartek/formacion/spring/springmvcbasico/SpringformulariosvalidacionApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springmvcbasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringformulariosvalidacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringformulariosvalidacionApplication.class, args);
+	}
+
+}
