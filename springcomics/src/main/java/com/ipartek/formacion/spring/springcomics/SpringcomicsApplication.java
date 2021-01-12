912a347e1db27797b62fa1bd6c81c8909a65d4e4
Javier Lete <javier@eskura21.com>
2021-01-12 15:47:40 +0100

    Inicio springcomics

---
+++ springcomics/src/main/java/com/ipartek/formacion/spring/springcomics/SpringcomicsApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springcomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcomicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcomicsApplication.class, args);
+	}
+
+}
---
+++ springcomics/src/test/java/com/ipartek/formacion/spring/springcomics/SpringcomicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springcomics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcomicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
