2168b89bb20f5758d6074fadf7098299071421c9
Javier Lete <javier@eskura21.com>
2021-01-13 17:56:55 +0100

    Seguridad por base de datos en JDBC

--- springcomics/src/main/java/com/ipartek/formacion/spring/springcomics/configuraciones/WebSecurityConfig.java
+++ springcomics/src/main/java/com/ipartek/formacion/spring/springcomics/configuraciones/WebSecurityConfig.java
@@ -1,21 +1,22 @@
 package com.ipartek.formacion.spring.springcomics.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-	
 	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -38,17 +39,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
 				.permitAll();
 	}
 
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		@SuppressWarnings("deprecation")
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("contra")
-				.roles("USER")
-				.build();
+	@Autowired
+	private DataSource dataSource;
 
-		return new InMemoryUserDetailsManager(user);
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
 	}
 }
