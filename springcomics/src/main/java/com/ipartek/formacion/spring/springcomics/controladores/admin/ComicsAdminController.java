534717771f7f5395a8022080384d7d1e16ab0758
Javier Lete <javier@eskura21.com>
2021-01-18 16:28:34 +0100

    Paso de listado a pantalla de edición con añadir

--- springcomics/src/main/java/com/ipartek/formacion/spring/springcomics/controladores/admin/ComicsAdminController.java
+++ springcomics/src/main/java/com/ipartek/formacion/spring/springcomics/controladores/admin/ComicsAdminController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ipartek.formacion.spring.springcomics.entidades.Comic;
@@ -15,19 +16,34 @@ import com.ipartek.formacion.spring.springcomics.repositorios.Dao;
 public class ComicsAdminController {
 	@Autowired
 	private Dao<Comic> dao;
-	
-	@GetMapping //("/comics")
+
+	@GetMapping // ("/comics")
 	public String comics(Model modelo) {
 		modelo.addAttribute("comics", dao.obtenerTodos());
 		return "admin/comics";
 	}
-	
+
 	@GetMapping("/{id}")
 	public String comic(@PathVariable Long id, Model modelo) {
-		Comic comic = dao.obtenerPorId(id);
-		
+		Comic comic;
+		if (id != 0) {
+			comic = dao.obtenerPorId(id);
+		} else {
+			comic = new Comic();
+		}
+
 		modelo.addAttribute("comic", comic);
-		
+
 		return "admin/comic";
 	}
+	
+	@PostMapping
+	public String comicPost(Model modelo) {
+		Comic comic = new Comic();
+
+		modelo.addAttribute("comic", comic);
+
+		return "admin/comic";
+	}
+	
 }
