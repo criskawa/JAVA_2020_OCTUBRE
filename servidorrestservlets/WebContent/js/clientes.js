e775be08a965730bfcf254d7bb442f8bb8f56bc1
Javier Lete <javier@eskura21.com>
2021-01-29 20:07:57 +0100

    Peticiones fetch con errores.

--- servidorrestservlets/WebContent/js/clientes.js
+++ servidorrestservlets/WebContent/js/clientes.js
@@ -1,4 +1,5 @@
-const URL = '/servidorrestservlets/api/clientes/';
+const URL = '/servidorrestservlets/api/v2/clientes/';
+const headers = { 'Content-Type': 'application/json' };
 
 window.onload = function() {
 
@@ -7,35 +8,54 @@ window.onload = function() {
 	$('form')[0].onsubmit = function(e) {
 		e.preventDefault();
 
-		$('table')[0].style = 'display: block';
-		$('form')[0].style = 'display: none';
-
-		const id = parseInt($('#id').value);
-		const nombre = $('#nombre').value;
-		const apellidos = $('#apellidos').value;
-
-		const cliente = { id, nombre, apellidos };
-
-		console.log(cliente, typeof cliente);
-
-		if (id) {
-			fetch(URL + id, { method: 'PUT', body: JSON.stringify(cliente) }).then(() => listar());
-		} else {
-			fetch(URL, { method: 'POST', body: JSON.stringify(cliente) }).then(() => listar());
-		}
+		enviarFormulario()
 	}
 
 	$('#a-insertar').onclick = function(e) {
 		e.preventDefault();
 
-		$('table')[0].style = "display: none";
-		$('form')[0].style = "display: block";
-
-		$('#id').value = '';
-		$('#nombre').value = '';
-		$('#apellidos').value = '';
+		mostrarFormulario();
 	}
 };
+
+function mostrarFormulario() {
+	$('table')[0].style = "display: none";
+	$('form')[0].style = "display: block";
+
+	$('#id').value = '';
+	$('#nombre').value = '';
+	$('#apellidos').value = '';
+}
+
+function enviarFormulario() {
+	
+	const id = parseInt($('#id').value);
+	const nombre = $('#nombre').value;
+	const apellidos = $('#apellidos').value;
+
+	const cliente = { id, nombre, apellidos };
+
+	console.log(cliente, typeof cliente);
+
+	if (id) {
+		fetch(URL + id, { method: 'PUT', body: JSON.stringify(cliente), headers }).then(response => {
+			if (response.ok) {
+				listar();
+			} else {
+				alert('Error');
+			}
+		});
+	}
+	else {
+		fetch(URL, { method: 'POST', body: JSON.stringify(cliente), headers }).then(response => {
+			if (response.ok) {
+				listar();
+			} else {
+				alert('Error');
+			}
+		});
+	}
+}
 
 async function listar() {
 	const respuesta = await fetch(URL);
@@ -63,7 +83,9 @@ async function listar() {
 		tbody.appendChild(fila);
 	});
 
+	$('table')[0].style = 'display: block';
 	$('form')[0].style = 'display: none';
+
 }
 
 async function editar(id) {
