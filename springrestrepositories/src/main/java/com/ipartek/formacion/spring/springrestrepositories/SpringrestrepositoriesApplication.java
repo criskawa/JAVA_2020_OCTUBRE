2e6857dfc6434745c370053493dcf038a9abb7ed
Javier Lete <javier@eskura21.com>
2021-02-04 16:09:18 +0100

    Inicio springrestrepositories

---
+++ springrestrepositories/src/main/java/com/ipartek/formacion/spring/springrestrepositories/SpringrestrepositoriesApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.springrestrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestrepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestrepositoriesApplication.class, args);
+	}
+
+}
