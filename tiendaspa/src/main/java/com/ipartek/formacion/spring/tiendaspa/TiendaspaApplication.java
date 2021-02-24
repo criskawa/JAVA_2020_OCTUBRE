b6041c7833e96625c089407d9cf844b83203e661
Javier Lete <javier@eskura21.com>
2021-02-24 17:37:51 +0100

    Inicio tienda

---
+++ tiendaspa/src/main/java/com/ipartek/formacion/spring/tiendaspa/TiendaspaApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.spring.tiendaspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaspaApplication.class, args);
+	}
+
+}
