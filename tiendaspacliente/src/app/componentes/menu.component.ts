49ff2baa10ee2ddbfbb5e8b4d3410eb12071b74e
Javier Lete <javier@eskura21.com>
2021-02-26 19:09:40 +0100

    Variación con pipe async

--- tiendaspacliente/src/app/componentes/menu.component.ts
+++ tiendaspacliente/src/app/componentes/menu.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 import { Categoria } from "../modelos/categoria";
 import { CategoriaService } from "../servicios/categoria.service";
 
 @Component({
     selector: 'app-menu',
     template: `
-        <ul *ngFor="let categoria of categorias">
+        <ul *ngFor="let categoria of categorias$ | async">
             <li>{{categoria.nombre}}: {{categoria.descripcion}}</li>
         </ul>
         `,
     styles: [ 'ul { background: lightgray }' ]
 })
 export class MenuComponent implements OnInit {
+    categorias$: Observable<Categoria[]>;
     categorias: Categoria[];
     
     constructor(public categoriaService: CategoriaService) {}
     
     ngOnInit(): void {
-        this.categoriaService.obtenerCategorias().subscribe(datos => this.categorias = datos);
+        this.categorias$ = this.categoriaService.obtenerCategorias(); //.subscribe(datos => this.categorias = datos);
     }
 
 
