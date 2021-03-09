0c5c28b528f3704eb3d245fe4fb067398a19321d
Javier Lete <javier@eskura21.com>
2021-03-09 17:18:41 +0100

    Rutados con hijos

--- angularejemplos/src/app/app-routing.module.ts
+++ angularejemplos/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HijoComponent } from './hijo/hijo.component';
+import { PadreComponent } from './padre/padre.component';
 
-const routes: Routes = [];
+const hijos = [
+  { path: 'hijo/:id/:version', component: HijoComponent }
+];
+
+const routes: Routes = [
+  { path: 'padre', component:PadreComponent, children: hijos },
+  { path: 'padre/:id', component: PadreComponent, children: hijos }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
